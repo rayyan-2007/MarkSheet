0a7b11d45c16b0062238276d8b16755860c3260f
rayyan-2007 <mrefxeditz@gmail.com>
2026-05-06 22:06:51 +0530

    Resubmit task

--- Script.js
+++ Script.js
@@ -1,87 +1,118 @@
-let a=10;
-let b = 0;
-let operator = "/";
+//let a=10;
+//let b = 0;
+//let operator = "/";
 
-switch(operator){
-    case "+":
-        console.log(a+b);
-        break;
-        case "-":
-            console.log(a-b);
-            break;
-            case"*":
-            console.log(a*b);
-            break;
-            case"/":
-            a !==0 && b!==0 ? console.log(a/b) : console.log("Division by zero is not allowed");
-            break;
-            default:
-                console.log("Invalid operator");
-}
+//switch(operator){
+//case "+":
+//console.log(a+b);
+//break;
+//case "-":
+//console.log(a-b);
+//break;
+//case"*":
+//console.log(a*b);
+//break;
+//case"/":
+//a !==0 && b!==0 ? console.log(a/b) : console.log("Division by zero is not allowed");
+//break;
+//default:
+// console.log("Invalid operator");
+//}
 
+//Create a variable `marks` to store student marks (0–100).
 
+// Add bonus marks using an assignment operator (+=).
 
-let marks = 0;
+// Determine the grade using if-else:
+// 90–100 → Grade A
+// 70–89 → Grade B
+// 50–69 → Grade C
+// Below 50 → Grade F
+
+// Determine Pass/Fail using a ternary operator:
+// (marks >= 50 ? "Pass" : "Fail")
+
+// Give remarks based on grade using a switch statement:
+// A → "Excellent!"
+// B → "Good"
+// C → "Average"
+// F → "Needs Improvement"
+
+// Print the results in the console:
+// Marks, Grade, Result, Remark
+
+let marks = 60;
 let bonusMarks = 0;
 let grade;
 let result;
 let remarks;
+let totalmarks = 0;
 
-if(marks<0 || marks>100){
-    console.log("You Entered a Invalid Mark !")
+if (marks <= 0) {
+  console.log("invalid mark");
+} else if (marks >= 90 && marks <= 100) {
+  grade = "A";
+  bonusMarks += 3;
+  totalmarks = marks + bonusMarks;
+  result = "passed";
+  console.log("marks:", marks);
+  console.log("grade:", grade);
+  console.log("result:", result);
+  console.log("bonusMarks:", bonusMarks);
+  console.log("totalmarks:", totalmarks);
+} 
+else if (marks >= 70 && marks <= 89) {
+  grade = "B";
+  bonusMarks += 2;
+  result = "passed";
+  totalmarks = marks + bonusMarks;
+  console.log("marks:", marks);
+  console.log("grade:", grade);
+  console.log("result:", result);
+  console.log("bonusMarks:", bonusMarks);
+  console.log("totalmarks:", totalmarks);
+} 
+else if (marks >= 50 && marks <= 69) {
+  grade = "C";
+  bonusMarks += 1;
+  result = "passed";
+  totalmarks = marks + bonusMarks;
+  console.log("marks:", marks);
+  console.log("grade:", grade);
+  console.log("result:", result);
+  console.log("bonusMarks:", bonusMarks);
+  console.log("totalmarks:", totalmarks);
+} 
+else if (marks >= 50 ? (result = "Passed") : (result = "Failed")) {
+  grade = "F";
+  totalmarks = marks;
+  console.log("marks:", marks);
+  console.log("grade:", grade);
+  console.log("result:", result);
+  console.log("totalmarks:", totalmarks);
 }
 
-else if(marks>=85 && marks<=100){
-    grade="A";
-    result = "passed"
-    bonusMarks = bonusMarks + 1;
-    marks = marks + bonusMarks;
-    switch(grade){
-        case "A":
-              console.log("High Marks with bonus: " + marks);
-              console.log("Grade: " + grade);
-              console.log("Result : " + result);
-              remarks = "Excellent";
-              console.log("Remarks: " + remarks);
-               break;
-    }
-  
+switch (grade) {
+  case "A":
+    remarks = "Excellent";
+    console.log("remarks:", remarks);
+    break;
+
+  case "B":
+    remarks = "Good";
+    console.log("remarks:", remarks);
+    break;
+
+  case "C":
+    remarks = "Average";
+    console.log("remarks:", remarks);
+    break;
+
+  case "F":
+    remarks = "Need Improvement";
+    console.log("remarks:", remarks);
+    break;
+
+  default:
+    console.log("invalid grade value");
 }
-else if(marks>=70 && marks<=89){
-    result = "passed";
-    grade="B";
-
-    console.log("Marks :" + marks);
-    console.log("Grade: " + grade);
-    console.log("Result: " + result);
-    console.log("Remarks: Good");
-    }
-
-
-
-else if(marks>=50 && marks<=69){
-    result = "passed"
-    grade="C";
-    console.log("Marks: " + marks);
-    console.log("Grade: " + grade);
-    console.log("Result: " + result);
-    console.log("Remarks: Average");
-    }
-    
-
-   else if(marks<=49){
-     result = "Failed";
-     grade = "F";
-    console.log("Marks: " + marks);
-    console.log("Grade: " + grade);
-    console.log("Result: " + result);
-    console.log("Remarks: Poor");
-}
-
-
-  else{
-    console.log("Invalid Input!")
-     }
-    
-
-
